782e662d76e8ee4cf7eac40866a75e3318f5e860
utsav wagle <utsavwagle508@gmail.com>
2023-11-02 19:27:51 +0545

    course section

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,3 +8,8 @@
      const isOpen = dropDownMenu.classList.contains("open");
      toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
  };
+
+
+ function redirectToNextPage() {
+    window.location.href = "../contact.html";
+}
